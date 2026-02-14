a74af476d1a2a3a1e60787c888f6b0679fb50579
Ganesh-DA <ganeshshimpi330@gmail.com>
2026-02-14 14:02:09 +0530

    Update config.py for new data/ and models/ paths

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 # File paths for data and saved models
-MEETS_DATA = 'meets.csv'
-LIFTING_DATA = 'openpowerlifting.csv'
+MEETS_DATA = 'data/meets.csv'
+LIFTING_DATA = 'data/openpowerlifting.csv'
 
-MODEL_FILE = 'powerlifting_model.pkl'
-FEATURES_FILE = 'feature_names.pkl'
+MODEL_FILE = 'models/powerlifting_model.pkl'
+FEATURES_FILE = 'models/feature_names.pkl'
 
 # Training settings
 SAMPLE_SIZE = 50000  # Number of rows to train on (increase for more accuracy)
